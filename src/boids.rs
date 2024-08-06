7bf156ef538aa6d48db461d4f2b60f36f65bbd3f
redpz <sv_redpz@outlook.com>
2024-08-06 15:33:30 +0100

    figured itout

--- src/boids.rs
+++ src/boids.rs
@@ -1,20 +1,19 @@
-const BOID_COUNT: usize = 10;
+use rand::{Rng, RngCore};
+
+const BOID_COUNT: usize = 100;
 use bevy::{
     prelude::*, render::render_resource::encase::internal::ReadFrom, sprite::MaterialMesh2dBundle,
 };
 
-#[derive(Component)]
+#[derive(Component, Clone)]
 pub struct Boid;
 
-#[derive(Component)]
-pub struct Position(pub Vec2);
-#[derive(Component)]
+#[derive(Component, Clone)]
 pub struct Velocity(pub Vec2);
 
 #[derive(Bundle)]
 pub struct BoidBundle {
     marker: Boid,
-    position: Position,
     velocity: Velocity,
     mesh_bundle: MaterialMesh2dBundle<ColorMaterial>,
 }
@@ -23,8 +22,7 @@ impl Default for BoidBundle {
     fn default() -> Self {
         Self {
             marker: Boid,
-            position: Position(Vec2::ZERO),
-            velocity: Velocity(Vec2 { x: 0., y: -1. }),
+            velocity: Velocity(Vec2 { x: 0., y: 0. }),
             mesh_bundle: MaterialMesh2dBundle::default(),
         }
     }
@@ -35,15 +33,47 @@ fn spawn_boid(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn((BoidBundle {
-        mesh_bundle: MaterialMesh2dBundle {
-            mesh: meshes.add(Triangle2d::default()).into(),
-            transform: Transform::default().with_scale(Vec3::new(15., 20., 0.)),
-            material: materials.add(Color::WHITE),
+    // TODO: this is a stupid way of doing it, do it with resources
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..BOID_COUNT {
+        commands.spawn((BoidBundle {
+            velocity: Velocity(
+                Vec2 {
+                    x: rng.gen_range(-1.0..1.0),
+                    y: rng.gen_range(-1.0..1.0),
+                    /* x: -1.0,
+                    y: -1.0, */
+                }
+                .normalize_or_zero(),
+            ),
+
+            mesh_bundle: MaterialMesh2dBundle {
+                mesh: meshes
+                    .add(Triangle2d {
+                        vertices: [
+                            Vec2::new(-5.0, -7.5),
+                            Vec2::new(5.0, -7.5),
+                            Vec2::new(0.0, 7.5),
+                        ],
+                    })
+                    .into(),
+                transform: Transform::from_xyz(
+                    rng.gen_range(-100.0..100.0),
+                    rng.gen_range(-100.0..100.0),
+                    rng.gen_range(-1.0..1.0),
+                )
+                .with_scale(Vec3::new(1., 1., 0.)),
+                material: materials.add(Color::srgb(
+                    rng.gen_range(0.0..1.0),
+                    rng.gen_range(0.0..1.0),
+                    rng.gen_range(0.0..1.0),
+                )),
+                ..default()
+            },
             ..default()
-        },
-        ..default()
-    },));
+        },));
+    }
 }
 
 fn setup(mut commands: Commands) {
@@ -57,22 +87,63 @@ fn setup(mut commands: Commands) {
     });
 }
 
-fn tick_boids(mut query: Query<(&mut Position, &mut Velocity, &mut Transform), With<Boid>>) {
-    for (mut position, mut velocity, mut transform) in &mut query {
-        position.0 += velocity.0;
-        transform.translation.x += velocity.0.x;
-        transform.translation.y += velocity.0.y;
+fn tick_boids(
+    mut query: Query<(&mut Velocity, &mut Transform), With<Boid>>,
+    boids_settings: Res<BoidsSettings>,
+) {
+    // NOTE: different implementations for holding a copy:
+    // - There isn't necessarily a need to copy although that would lead to non deterministic
+    // behaviour.
+    // Read https://discord.com/channels/691052431525675048/1270369289785839667/1270381052237709332
 
-        // update rotation to point towards direction velocity points to
-        // transform.rotation.z = std::f32::consts::PI;
+    let copy: Vec<(Velocity, Vec3)> = query
+        .iter()
+        .map(|r| {
+            let velocity = r.0.clone();
+            let transform = r.1.translation.clone();
+            (velocity, transform)
+        })
+        .collect();
+
+    for (mut vel_tuple, mut transform) in query.iter_mut() {
+        // update positions
+        // this is just to make the syntax a little cleaner
+        let position = &mut transform.translation;
+        let velocity = &mut vel_tuple.0;
+        position.x += velocity.x;
+        position.y += velocity.y;
+
+        let mut avg_vel = Vec2::splat(0.0);
+        let mut boids_invision = 0;
+
+        // iterate over the copy and if in vision range then add to average vel
+        for (copy_velocity, position) in copy.iter() {
+            avg_vel += copy_velocity.0;
+            boids_invision += 1;
+        }
+
+        if boids_invision != 0 {
+            avg_vel /= boids_invision as f32;
+        }
+
+        // for some reason the qat is rotating Ccw so you must negate it
+        // Calculate the angle using SOHCAHTOA. Since we want angle from the
+        let mut angle = -(velocity.x / velocity.y).atan();
+        // reasoning for this being done so is that when Y is negative we get an angle from the
+        // nagative Y axis, when we are looking for the positive Y axis angle difference. In such
+        // cases you should just flip it by adding 180 degrees (or PI in radians)
+        if velocity.y < 0.0 {
+            angle += std::f32::consts::PI;
+        }
+        transform.rotation = Quat::from_rotation_z(angle);
     }
 }
 
-fn print_boids(query: Query<(Entity, &Position, &Velocity), With<Boid>>) {
-    for (e_id, position, velocity) in &query {
+fn print_boids(query: Query<(Entity, &Transform, &Velocity), With<Boid>>) {
+    for (e_id, transform, velocity) in &query {
         info!(
             "{}\n-----------\nPos:{}\nVel:{}\n",
-            e_id, position.0, velocity.0
+            e_id, transform.translation, velocity.0
         );
     }
 }
@@ -87,10 +158,9 @@ pub struct BoidsPlugin;
 
 impl Plugin for BoidsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup)
+        app.insert_resource(Time::<Fixed>::from_hz(60.)) //update 60 times per second
+            .add_systems(Startup, setup)
             .add_systems(Startup, spawn_boid)
-            .insert_resource(Time::<Fixed>::from_hz(60.)) //update 60 times per second
-            .add_systems(FixedUpdate, (tick_boids, print_boids).chain());
-        //                       seperate print boids later           ^^^^^^^^^^^
+            .add_systems(FixedUpdate, tick_boids);
     }
 }
