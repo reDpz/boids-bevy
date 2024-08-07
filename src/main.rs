c4a7d8c3e6c2696580a531f9059645fb3ff7d1c5
redpz <sv_redpz@proton.me>
2024-08-07 22:16:26 +0100

    removed unnecessary resource

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,6 @@ fn main() {
     App::new()
         // why is it not black by default?
         .insert_resource(ClearColor(Color::BLACK))
-        .insert_resource(AmbientLight {
-            color: Color::WHITE,
-            brightness: 0.75,
-        })
         .add_plugins(DefaultPlugins)
         .add_plugins(BoidsPlugin)
         .run();
