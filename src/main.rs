d4a757735250e1c0b3228a3c5cd5bee7e4a78e21
redpz <sv_redpz@proton.me>
2024-08-07 22:27:10 +0100

    change backend API

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 mod boids;
 use boids::*;
 
-use bevy::{prelude::*, utils::info};
+use bevy::{prelude::*, render::{settings::{Backends, RenderCreation, WgpuSettings}, RenderPlugin}, utils::info};
 
 fn main() {
     App::new()
         // why is it not black by default?
         .insert_resource(ClearColor(Color::BLACK))
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(
+            // use VULKAN instead of DX12/DX11
+            RenderPlugin{
+                render_creation: WgpuSettings{
+                    backends: Some(Backends::VULKAN),..default()
+                }.into(),..default()
+        }))
         .add_plugins(BoidsPlugin)
         .run();
     println!("Hello, world!");
